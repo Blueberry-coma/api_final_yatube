76be1ccb18da3e4c8111e8a84424ccdb42c352e8
Александр Овчинников <alex-ovchinnikov@mail.ru>
2023-04-05 12:06:00 +0300

    9 sprint 2 v

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from .permissions import IsAuthorOrReadOnly
 from posts.models import Group, Post
@@ -18,16 +17,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied("Нельзя изменять этот контент")
-        return super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied("Нельзя удалять этот контент")
-        return super().perform_destroy(instance)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -52,18 +41,6 @@ class CommentViewSet(viewsets.ModelViewSet):
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, id=post_id)
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author == self.request.user:
-            serializer.save()
-        else:
-            raise PermissionDenied("Нельзя редактировать этот комментарий")
-
-    def perform_destroy(self, instance):
-        if instance.author == self.request.user:
-            instance.delete()
-        else:
-            raise PermissionDenied("Вы не можете удалить этот комментарий")
 
 
 class FollowViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
